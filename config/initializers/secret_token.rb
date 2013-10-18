fdc52d84dbae1bd79a6410ce71e7908bff01ff21
Takaaki Muta <takaaki.muta@gmail.com>
2013-10-19 00:30:20 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fly::Application.config.secret_key_base = '48b15901148ab14b6e147c0f742cc19fa1ea14b4d0c23bc5fe931da92e582efce6553573f31d08ff5bf6f30b3c3a8fbfa2cda0fc9ecb1ed6e5987fb2d584c172'
