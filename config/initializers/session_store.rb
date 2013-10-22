5a19c70a32bdc1a78ec8ddba8cb0488f7572d77a
Takaaki Muta <takaaki.muta@gmail.com>
2013-10-22 22:53:49 +0900

    セッションの有効時間を30分に設定

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Fly::Application.config.session_store :cookie_store, key: '_fly_session'
+Fly::Application.config.session_store :cookie_store, key: '_fly_session', expire_after: 30.minutes
