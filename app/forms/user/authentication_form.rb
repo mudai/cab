f110cec38a421ded931457c5db501618a71a6a5a
Takaaki Muta <takaaki.muta@gmail.com>
2013-10-22 13:43:28 +0900

    試行錯誤中

--- app/forms/user/authentication_form.rb
+++ app/forms/user/authentication_form.rb
@@ -11,17 +11,15 @@ class AuthenticationForm
 
   # ログインボタンを押されたときの処理
   def submit(params)
-    self.org_dir = params[:org_dir]
-    self.login_id = params[:login_id]
-    self.password = params[:password]
+    self.org_dir, self.login_id, self.password = params[:org_dir], params[:login_id], params[:password]
     auth = AuthenticationService.new(org_dir, login_id, password)
 
-    # login_id, passwordの入力チェック, 認証可能かどうか
+    # org_dir, login_id, passwordの入力チェック, 認証可能かどうか
     if valid? && auth.authenticate?
       @user = auth.user
-      # TODO: ログインログ書き込み等の処理をこの辺に入れる
+      auth.after_authenticate!
       true
-    else # 入力チェックエラーの場合は一律のエラーメッセージを出す
+    else # 入力チェックエラーの場合は一律のエラーメッセージをコントローラー側で出す
       false
     end
   end
--- app/services/authentication_service.rb
+++ app/services/authentication_service.rb
@@ -1,15 +1,22 @@
 class AuthenticationService
+  # 認証処理を担うクラス
   def initialize(org_dir, login_id, password)
     @org_dir, @login_id, @password = login_id, password, org_dir
   end
 
+  # 認証が可能なユーザーを取得
   def user
     @user ||= user_with_password
   end
 
-  # 認証する
+  # 認証可能かどうかを判断
   def authenticate?
     user.present?
+  end
+
+  # ログイン後の処理
+  def after_authenticate!
+    # 認証後のログ書き込み処理を入れる
   end
 
   private
