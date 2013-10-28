e4373cddde5745716691fb86f0b9f9bb90521009
Takaaki Muta <takaaki.muta@gmail.com>
2013-10-29 00:02:03 +0900

    コードを修正

--- app/controllers/user/base_controller.rb
+++ app/controllers/user/base_controller.rb
@@ -30,7 +30,7 @@ class User::BaseController < ApplicationController
 
   def current_user
     if !request.ssl? || cookies.signed[:secure_user_id] == "fly_secure_key_#{session[:user_id]}"
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      @current_user ||= User.includes(:organization).find(session[:user_id]) if session[:user_id]
     end
   end
   helper_method :current_user
