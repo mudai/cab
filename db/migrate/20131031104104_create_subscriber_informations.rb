98a65b9ab22e85da6dacf0dc8851bebe8bbbcc65
Takaaki Muta <takaaki.muta@gmail.com>
2013-11-02 00:39:26 +0900

    加入者データ管理テーブルを作成

--- db/migrate/20131031104104_create_subscriber_informations.rb
+++ db/migrate/20131031104104_create_subscriber_informations.rb
@@ -2,7 +2,15 @@ class CreateSubscriberInformations < ActiveRecord::Migration
   def change
     # 健保加入者情報を管理するテーブル
     create_table :subscriber_informations do |t|
-
+      t.integer :organization_id
+      t.integer :user_id
+      t.string :family_name # 名字
+      t.string :first_name # 名前
+      t.string :family_name_kana
+      t.string :first_name_kana
+      t.string :code # 記号
+      t.string :number # 番号
+      t.datetime :associated_at # ひも付け日時
       t.timestamps
     end
   end
