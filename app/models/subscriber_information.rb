154e35843e268fc7a38b3222da72d440e7e11ca1
Takaaki Muta <takaaki.muta@gmail.com>
2013-11-19 19:16:54 +0900

    少し整理

--- app/models/subscriber_information.rb
+++ app/models/subscriber_information.rb
@@ -2,4 +2,25 @@ class SubscriberInformation < ActiveRecord::Base
   belongs_to :organization
   belongs_to :user
   has_many :provisional_users
+
+  # where or all スコープの中から適用情報が一致するユーザーを取り出す
+  def self.search(code: code, number: number, birthday: birthday, family_name_kana: family_name_kana, first_name_kana: first_name_kana)
+    scope = self
+    scope = scope.where(code: code, number: number, birthday: birthday) # indexの効く部分で絞り込む
+
+    # 文字列を揃えマッチング
+    info = scope.to_a.find do |x|
+      convert_kana(x.family_name_kana) == convert_kana(family_name_kana) &&
+        convert_kana(x.first_name_kana) == convert_kana(first_name_kana)
+    end
+
+    info
+  end
+
+  private
+  # そのうちモジュールに切り出してextendする
+  def self.convert_kana(str)
+    return "" if str.blank?
+    NKF.nkf("-w8Z4x", str).tr("ｧｨｩｪｫｯｬｭｮ","ｱｲｳｴｵﾂﾔﾕﾖ").tr("ﾞ","").tr("ﾁｽ","ｼﾂ")
+  end
 end
--- app/validators/subscription_validator.rb
+++ app/validators/subscription_validator.rb
@@ -2,36 +2,23 @@ class SubscriptionValidator < ActiveModel::Validator
   def validate(r)
     org = ::Organization.find_by(host: r.host) # hostが存在することは他のバリデータで行う
 
-    subscription_error = false
-    subscriptions = org.subscriber_informations.where(code: r.code, number: r.number, birthday: r.birthday)
+    info = org.subscriber_informations.search(
+      code: r.code,
+      number: r.number,
+      birthday: r.birthday,
+      family_name_kana: r.family_name_kana,
+      first_name_kana: r.first_name_kana 
+    )
 
-    if subscriptions.count == 0
-      # 適用データが見つかりません。
-      subscription_error = true
-    end
-
-    unless subscription = subscriptions.to_a.find {|x|
-      convert_kana(x.family_name_kana) == convert_kana(r.family_name_kana) && convert_kana(x.first_name_kana) == convert_kana(r.first_name_kana)
-    }
-      # 適用データが見つかりません。
-      subscription_error = true
-    end
-
-    if subscription_error
+    if info.nil?
       message = I18n.t('activerecord.errors.messages.not_found')
       r.errors.add(:subscriber_information, message)
     end
 
-    if subscription.try(:user).try(:present?)
+    if info.try(:user).try(:present?)
       # この適用情報で既にサインアップ済み
       message = I18n.t('activerecord.errors.messages.already_used')
       r.errors.add(:subscriber_information, message)
     end
-  end
-
-  private
-  def convert_kana(str)
-    return "" if str.blank?
-    ::NKF.nkf("-w8Z4x", str).tr("ｧｨｩｪｫｯｬｭｮ","ｱｲｳｴｵﾂﾔﾕﾖ").tr("ﾞ","").tr("ﾁｽ","ｼﾂ")
   end
 end
