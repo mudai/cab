f54004784df9089a2c8ca61189b28b75b68e2108
Takaaki Muta <takaaki.muta@gmail.com>
2013-11-29 16:09:24 +0900

    メールメッセージを作成中

--- app/mailers/user/registrations/definitive_mailer.rb
+++ app/mailers/user/registrations/definitive_mailer.rb
@@ -1,2 +1,26 @@
-class User::Registrations::DefinitiveMailer < ActionMailer::Base
+class User::Registrations::DefinitiveMailer < Jpmobile::Mailer::Base
+  default from: 'noreply@qupio.jp'
+
+  # テンプレートが無かった場合に利用されるデフォルトの件名
+  def default_subject
+    "[<%= attributes[:nickname] %>様] 本登録が完了しました" # yaml化？ TODO
+  end
+
+  def definitive_mail(attributes)
+    @attributes = attributes # デフォルトのerbテンプレート用
+    # templateが無い場合のデフォルトの文章はuser/registrations/definitive_mailer.text.erbが利用される
+
+    org = Organization.find_by(host: attributes[:host])
+    self.class.default_url_options[:host] = org.host
+    if org && template = org.mail_templates.find_by(code: "definitive_mail")
+      mail(
+        to: attributes[:email],
+        subject: ERB.new(template.subject).result(binding),
+        body: ERB.new(template.body).result(binding)
+      )
+    else
+      mail to: attributes[:email], subject: ERB.new(default_subject).result(binding)
+    end
+  end
+
 end
