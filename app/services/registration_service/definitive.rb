ca428143cd9fb5dfbdd55ee6c177175052cf6dbb
Takaaki Muta <takaaki.muta@gmail.com>
2013-11-29 16:59:32 +0900

    ユーザー登録フローの作成

--- app/services/registration_service/definitive.rb
+++ app/services/registration_service/definitive.rb
@@ -15,22 +15,27 @@ class RegistrationService::Definitive
 
     if onetime_token && onetime_token.expired_at >= Time.now # 有効期間内
 
+      # tokenから仮登録ユーザーを引いて、userを作成する
+      prov = onetime_token.provisional_user
+
       ActiveRecord::Base.transaction do
-        # tokenから仮登録ユーザーを引いて、userを作成する
-        prov = onetime_token.provisional_user
         user = org.users.new do |u|
           u.login_id = prov.login_id
           u.password_digest = prov.password_digest
         end
         user.build_profile.nickname = prov.nickname
         user.save!(validate: false) # パスワードは暗号化させたままそのまま格納
+        # subscriber_informationのassociated_atとuser_idを更新する
+        subscriber = prov.subscriber_information
+        subscriber.update!(user_id: user.id, associated_at: Time.now)
         # ユーザーの作成が問題なければ他のtokenを無効化する
         onetime_token.update!(status: false)
         # 作成したユーザーをconfirmed_userにセット
         @confirmed_user = user
       end
 
-      # 登録完了メールの送信 TODO: 
+      # 登録完了メールの送信
+      send_mail(prov.attributes.merge(host: host))
 
       true
     else # 有効期間外か見つからない
@@ -38,6 +43,10 @@ class RegistrationService::Definitive
       onetime_token.try(:update!, {status: false} )
       false
     end
+  end
+
+  def send_mail(attrs)
+    User::Registrations::DefinitiveMailer.definitive_mail(attrs.symbolize_keys).deliver
   end
 
   # confirmを押下して問題なければ対象ユーザーが取得できる
