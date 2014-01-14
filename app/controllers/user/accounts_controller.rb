a7719545e669546e9deeb0a359e80b3c3260097d
Takaaki Muta <takaaki.muta@gmail.com>
2014-01-15 00:04:21 +0900

    コメント追加

--- app/controllers/user/accounts_controller.rb
+++ app/controllers/user/accounts_controller.rb
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 #
 class User::AccountsController < User::BaseController
+
+  # /account
   def index
+  end
+
+  # /account/edit
+  def edit
+  end
+
+  # /account/update POST
+  def update
   end
 end
